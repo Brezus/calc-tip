527f7913d2b95c0b082cde5c17a3bc97f3ed89ba
millz <73348763+Brezus@users.noreply.github.com>
2021-10-21 15:34:26 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,142 +1,103 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-  let tipsGrid = document.getElementById("tip-grid");
-  let billErrorMsg = document.getElementById("first-error");
   let billInput = document.getElementById("bill-input");
-  let billInputVal = billInput.value;
   let personErrorMsg = document.getElementById("second-error");
   let personInput = document.getElementById("person-input");
-  let personInputVal = personInput.value;
   let customInput = document.getElementById("custom-input");
-  let customBox = document.querySelector(".custom-box");
-  let customErrorMsg = document.querySelector(".custom-input-error-msg");
-  let billInputInt;
-  let tipAmount;
-  let Total;
-  let formattedTarget;
-  let formattedTargetPercent;
-  let percentOfVal;
-  let percentOfValFormatted;
-  let myArray = [];
-  let reset = document.getElementById('reset');
-  let firstVal = document.querySelector(".tip-amt");
-  let lastVal = document.querySelector(".total-amt");
+  let reset = document.getElementById("reset");
+  let results = document.querySelectorAll('.amt')
+  let tips = document.querySelectorAll('.tip-box')
+  let tip = .15;
+  let total;
+  let bill = 0;
+  let headCount = 1;
 
-  reset.addEventListener('click', () => {
-    personInput.value = "";
-    billInput.value = "";
-    customInput.value = "";
-    firstVal.innerHTML = '$0.00'
-    lastVal.innerHTML = '$0.00'
-  })
-
-  customInput.addEventListener("input", function () {
-    checkInput(customInput, customErrorMsg);
-    let customValue = customInput.value;
-  });
-
-  personInput.addEventListener("input", function () {
-    checkInput(personInput, personErrorMsg);
-     function calcTip(baseBill, percentTip, headCount) {
-    if (headCount == 0) {
-      let tipAmount = baseBill * percentTip;
-      console.log(baseBill + " when headcount is false");
-      let tipAndBill = +tipAmount + +baseBill;
-      console.log(tipAndBill + " when headcount is false");
-      console.log(headCount);
-      displayCost(tipAmount, tipAndBill);
-      myArray = [tipAndBill, tipAmount]
-      return myArray;
-    } else {
-      let tipAmount = baseBill * percentTip;
-      console.log(baseBill);
-      let tipDivided = +tipAmount / +headCount;
-      console.log(tipDivided + " when headcount is true");
-      let tipAndBill = +baseBill + +tipAmount;
-      console.log(tipAndBill + " when headcount is true");
-      console.log(headCount);
-      displayCost(tipDivided, tipAndBill);
-      myArray = [tipAndBill, tipAmount]
-      return myArray;
-    }
+  function validateFloat(s) {
+      let rgx = /^[0-9]*\.?[0-9]*$/;
+      return s.match(rgx)
   }
-    calcTip(billInputVal, formattedTargetPercent, personInputVal);
-    displayCost(myArray[1], myArray[0])
-  });
+
+  function validateInt(s) {
+      let rgx = /^\d+$/;
+      return s.match(rgx)
+  }
+
 
   window.onload = function () {
-    // this function clears all data in all input fields
-    // https://stackoverflow.com/questions/38799096/clear-input-fields-on-page-refresh-microsoft-edge
     personInput.value = "";
     billInput.value = "";
     customInput.value = "";
   };
 
- 
-
-  function checkInput(input, msg) {
-    //check if value entered is not a number
-    if (isNaN(input.value)) {
-      //add error message
-      msg.style.display = "block";
-    } else {
-      //remove error message since it is a number
-      inputInt = Number(input.value);
-      msg.style.display = "none";
-    }
-  }
-
-  function displayCost(tipValue, totalValue) {
-    firstVal = document.querySelector(".tip-amt");
-    lastVal = document.querySelector(".total-amt");
-    firstVal.innerHTML = tipValue;
-    lastVal.innerHTML = totalValue;
-  }
-  tipsGrid.addEventListener("click", (e) => {
-    checkInput(billInput, billErrorMsg);
-    // checkInput(personInput, personErrorMsg) for the second input that has to be a number
-    if (
-      e.target.className === "tip-box" ||
-      e.target.className === "tip-box preset-box"
-    ) {
-      formattedTarget = e.target.innerHTML.slice(0, -1);
-      //   console.log(formattedTarget)
-      // the button selected looses its last element (%) with this slice function
-      formattedTargetPercent = formattedTarget / 100;
-      //   console.log(formattedTargetPercent)
-      // divide the number value of button selected which is a str by 100 to calculate the percent value and turn to int
-      percentOfVal = formattedTargetPercent * billInput.value;
-      //   console.log(percentOfVal)
-      // multiply the percent value by the value given by the user to find the percent of that give value
-      percentOfValFormatted = Number(percentOfVal.toFixed(2));
-      //   console.log(percentOfValFormatted + 'percent of value formatted')
-      //here we use toFixed function to remove excessive zeroes after the decimal point
-    } else if (e.target.className === "tip-box custom-box") {
-      // here we check if the custom button has been clicked
-      // then make a boolean (customSelected)
-      let customSelected = true;
-      if (customSelected) {
-        // if the boolean (customSelected) is true
-        // we then make the custom button disappear
-        // and remove the hidden class from our input
-        // so the input takes the place of the custom button in the grid
-        // and then we set the boolean to false
-        e.target.style.display = "none";
-        customInput.classList.remove("hidden");
-        customInput.focus();
-
-        // use this focus function to activate (focus) the customInput as soon as the custom button is clicked
-        customSelected = false;
-        // let customPercent = customInput.value
-        // let customPercentFormatted = customPercent.slice(0, -1)
-        // console.log(customPercentFormatted)
-      } else {
-        // if the boolean is false
-        // we make the custom box button visible again
-        // we then make the input box hidden and set the boolean to false
-        customBox.style.display = "block";
-        customInput.classList.add("hidden");
-        customSelected = true;
-      }
-    }
+  reset.addEventListener("click", () => {
+    personInput.value = "";
+    billInput.value = "";
+    customInput.value = "";
+    firstVal.innerHTML = "$0.00";
+    lastVal.innerHTML = "$0.00";
   });
+
+  billInput.addEventListener("input", function () {
+    if (!validateFloat(billInput.value)) {
+        billInput.value = billInput.value.substring(0, billInput.value.length - 1)
+    }
+    bill = Number(billInput.value)
+    calculateTip()
+    // console.log(bill)
+  });
+  
+  customInput.addEventListener("input", function () {
+    if (!validateFloat(customInput.value)) {
+        customInput.value = customInput.value.substring(0, customInput.value.length - 1)
+    }
+    tip = customInput.value/ 100
+    tips.forEach(btn => {
+        btn.classList.remove('btn-active')
+    })
+    calculateTip()
+  });
+
+  personInput.addEventListener("input", function () {
+    if (!validateInt(personInput.value)) {
+        personInput.value = personInput.value.substring(0, personInput.value.length - 1)
+    }
+    headCount = parseFloat(personInput.value)
+    if (personInput.value <= 0) {
+        personErrorMsg.classList.remove('hidden')
+        setTimeout(function() {
+        personErrorMsg.classList.add('hidden')
+        }, 3000)
+    } 
+    calculateTip()
+    // console.log(headCount)
+  });
+
+  tips.forEach(btn => {
+      btn.addEventListener('click', findTip)
+  })
+
+  function findTip(event) {
+      tips.forEach(btn => {
+          btn.classList.remove('btn-active')
+          if (event.target.innerHTML == btn.innerHTML) {
+              btn.classList.add('btn-active');
+              // parseFloat gets rid of the percent sign at the end of the inner html of the button clicked
+              tip = parseFloat(btn.innerHTML)/100
+              console.log(tip)
+          }
+      })
+      calculateTip()
+      customInput.value = '';
+  }
+
+  function calculateTip() {
+      if (headCount >= 1) {
+          let tipAmount = bill * tip / headCount;
+          let total = (bill * tip) / headCount;
+          results[0].innerHTML = '$' + tipAmount.toFixed(2)
+          results[1].innerHTML = '$' + total.toFixed(2)
+      } else {
+        
+      }
+  }
+
 });
